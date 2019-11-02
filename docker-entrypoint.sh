30e93d75f87c96b201198ae8f0b46a1c9107b838
jimmy201602 <zhengge2012@gmail.com>
2019-11-02 21:16:01 +0800

    fix docker image bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ trap "kill -15 -1 && echo all proc killed" TERM KILL INT
 if [ "$1" = "start" ]; then
         service redis-server start
         service nginx start
-        /usr/local/bin/supervisord -c /etc/supervisord.conf
+        /usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
         sleep inf & wait
 else
         exec "$@"
